1328be3c7094e46ae99fe3ebef21eb82cb156e31
TayaHatum <21taya21@gmail.com>
2022-05-06 11:54:55 +0300

    added config

---
+++ src/test/java/tests/LaunchTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+import configuration.AppiumConfiguration;
+import org.testng.annotations.Test;
+
+public class LaunchTest extends AppiumConfiguration {
+
+    @Test
+    public void launchTest(){
+
+    }
+}
